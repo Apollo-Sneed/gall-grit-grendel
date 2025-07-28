5dd08b9f29a827e66d8ef918f6a7b32b1a9a3241
Apollo Sneed <apollo-sneed@users.noreply.github.com>
2025-07-28 00:41:06 -0500

    Remove sensitive and test files before public release

--- scripts/untitled folder/checkBalanceSepolia.js
+++
@@ -1,7 +0,0 @@
-const { ethers } = require("hardhat");
-async function main() {
-  const [signer] = await ethers.getSigners();
-  const bal = await ethers.provider.getBalance(signer.address);
-  console.log("ETH balance:", ethers.formatEther(bal), "ETH");
-}
-main();
